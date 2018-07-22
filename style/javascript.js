0a09c3f42e179929904bef1ff324f92cf410e41f
Ashikurrahman14514 <ashikurrahman14514@gmail.com>
2018-07-23 02:26:54 +0600

    hurray

--- style/javascript.js
+++ style/javascript.js
@@ -1,6 +1,8 @@
 function runall() {
-    var a  =document.getElementsByClassName("run")[0].value;
-    var b = document.getElementsByClassName("run")[1].value;
+    var p =document.getElementsByClassName("run")[0].value;
+    var a= parseInt(p);
+    var o = document.getElementsByClassName("run")[1].value;
+    var b=parseInt(o);
     var c = document.getElementsByClassName("run")[2].value;
     var d = document.getElementsByClassName("run")[3].value;
     var e = document.getElementsByClassName("run")[4].value;
@@ -10,6 +12,8 @@ function runall() {
     var i = document.getElementsByClassName("run")[8].value;
     var j = document.getElementsByClassName("run")[9].value;
     var k = document.getElementsByClassName("run")[10].value;
-    var l =a+b+c+d+e+f+g+h+i+j+k
+      var m = document.getElementsByClassName("run")[11].value;
+    
+    var l =a+b+c+d+e+f+g+h+i+j+k+m
     document.getElementsByClassName("allrun")[0].value=l;
 }
