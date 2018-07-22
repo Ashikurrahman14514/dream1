23eef73ce562a22ed569e79c0046c5a44055d4a4
Ashikurrahman14514 <ashikurrahman14514@gmail.com>
2018-07-23 02:02:24 +0600

    all run not work

---
+++ style/javascript.js
@@ -0,0 +1,15 @@
+function runall() {
+    var a  =document.getElementsByClassName("run")[0].value;
+    var b = document.getElementsByClassName("run")[1].value;
+    var c = document.getElementsByClassName("run")[2].value;
+    var d = document.getElementsByClassName("run")[3].value;
+    var e = document.getElementsByClassName("run")[4].value;
+    var f = document.getElementsByClassName("run")[5].value;
+    var g = document.getElementsByClassName("run")[6].value;
+    var h = document.getElementsByClassName("run")[7].value;
+    var i = document.getElementsByClassName("run")[8].value;
+    var j = document.getElementsByClassName("run")[9].value;
+    var k = document.getElementsByClassName("run")[10].value;
+    var l =a+b+c+d+e+f+g+h+i+j+k
+    document.getElementsByClassName("allrun")[0].value=l;
+}
