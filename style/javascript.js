05205f0e96322ea956ade591132e27ce17f002ad
Ashikurrahman14514 <ashikurrahman14514@gmail.com>
2018-07-24 01:51:07 +0600

    run addition complite hurry

--- style/javascript.js
+++ style/javascript.js
@@ -3,17 +3,45 @@ function runall() {
     var a= parseInt(p);
     var o = document.getElementsByClassName("run")[1].value;
     var b=parseInt(o);
-    var c = document.getElementsByClassName("run")[2].value;
-    var d = document.getElementsByClassName("run")[3].value;
-    var e = document.getElementsByClassName("run")[4].value;
-    var f = document.getElementsByClassName("run")[5].value;
-    var g = document.getElementsByClassName("run")[6].value;
-    var h = document.getElementsByClassName("run")[7].value;
-    var i = document.getElementsByClassName("run")[8].value;
-    var j = document.getElementsByClassName("run")[9].value;
-    var k = document.getElementsByClassName("run")[10].value;
-      var m = document.getElementsByClassName("run")[11].value;
+  var v = document.getElementsByClassName("run")[2].value;
+  var c = parseInt(v);
+  var w = document.getElementsByClassName("run")[3].value;
+  var d = parseInt(w);
+   var q = document.getElementsByClassName("run")[4].value;
+   var e =parseInt(q);
+    var t = document.getElementsByClassName("run")[5].value;
+    var f =parseInt(t);
+    var u = document.getElementsByClassName("run")[6].value;
+    var g =parseInt(u);
+    var h5= document.getElementsByClassName("run")[7].value;
+    var h=parseInt(h5);
+    var z = document.getElementsByClassName("run")[8].value;
+    var i = parseInt(z);
+     var s = document.getElementsByClassName("run")[9].value;
+     var j =parseInt(s);
+    var ml = document.getElementsByClassName("run")[10].value;
+     var k = parseInt(ml);
+      var jm = document.getElementsByClassName("run")[11].value;
+       var m =parseInt(jm);
     
-    var l =a+b+c+d+e+f+g+h+i+j+k+m
+    var l =a+b+c+d+e+f+g+h+i+j+k+m;
     document.getElementsByClassName("allrun")[0].value=l;
 }
+   
+   
+    // var q = document.getElementsByClassName("run")[4].value;
+    // var e =parseInt(q);
+    // var t = document.getElementsByClassName("run")[5].value;
+    // var f =parseInt(t);
+    // var u = document.getElementsByClassName("run")[6].value;
+    // var g =parseInt(u);
+    // var u = document.getElementsByClassName("run")[6].value;
+    // var g =parseInt(u);
+    // var z = document.getElementsByClassName("run")[8].value;
+    // var i = parseInt(z);
+    // var s = document.getElementsByClassName("run")[9].value;
+    // var j =parseInt(s);
+    // var g = document.getElementsByClassName("run")[10].value;
+    // var k = parseInt(g);
+    //   var jm = document.getElementsByClassName("run")[11].value;
+    //   var m =parseInt(jm);
