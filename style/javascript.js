6af480c6b5ba2d3460853fe70ac3e112fc5a13ff
Ashikurrahman14514 <ashikurrahman14514@gmail.com>
2018-08-12 03:00:16 +0600

    booll count hyca

--- style/javascript.js
+++ style/javascript.js
@@ -1,31 +1,30 @@
-function runall() {
-    var p =document.getElementsByClassName("run")[0].value;
+function bolllllall() {
+    var p =document.getElementsByClassName("over")[0].value;
     var a= parseInt(p);
-    var o = document.getElementsByClassName("run")[1].value;
+    var o = document.getElementsByClassName("over")[1].value;
     var b=parseInt(o);
-  var v = document.getElementsByClassName("run")[2].value;
+  var v = document.getElementsByClassName("over")[2].value;
   var c = parseInt(v);
-  var w = document.getElementsByClassName("run")[3].value;
+  var w = document.getElementsByClassName("over")[3].value;
   var d = parseInt(w);
-   var q = document.getElementsByClassName("run")[4].value;
+   var q = document.getElementsByClassName("over")[4].value;
    var e =parseInt(q);
-    var t = document.getElementsByClassName("run")[5].value;
+    var t = document.getElementsByClassName("over")[5].value;
     var f =parseInt(t);
-    var u = document.getElementsByClassName("run")[6].value;
+    var u = document.getElementsByClassName("over")[6].value;
     var g =parseInt(u);
-    var h5= document.getElementsByClassName("run")[7].value;
+    var h5= document.getElementsByClassName("over")[7].value;
     var h=parseInt(h5);
-    var z = document.getElementsByClassName("run")[8].value;
+    var z = document.getElementsByClassName("over")[8].value;
     var i = parseInt(z);
-     var s = document.getElementsByClassName("run")[9].value;
+     var s = document.getElementsByClassName("over")[9].value;
      var j =parseInt(s);
-    var ml = document.getElementsByClassName("run")[10].value;
+    var ml = document.getElementsByClassName("over")[10].value;
      var k = parseInt(ml);
-      var jm = document.getElementsByClassName("run")[11].value;
-       var m =parseInt(jm);
+     
     
-    var l =a+b+c+d+e+f+g+h+i+j+k+m;
-    document.getElementsByClassName("allrun")[0].value=l;
+    var l =a+b+c+d+e+f+g+h+i+j+k;
+    document.getElementsByClassName("allbolll")[0].value = l;
 }
 
 
